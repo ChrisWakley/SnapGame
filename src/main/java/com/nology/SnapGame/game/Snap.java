074a2e3ac78556e23646b700e12b15e037587efe
Home <chris.wakley666@gmail.com>
2022-11-25 16:53:01 +0000

    Game ends when a snap occurs.

--- src/main/java/com/nology/SnapGame/game/Snap.java
+++ src/main/java/com/nology/SnapGame/game/Snap.java
@@ -17,7 +17,13 @@ public class Snap extends CardGame {
             String playerOne = TextInterraction.playerOne;
             if (input.isEmpty()) {
                 dealCard(playDeck);
+                card++;
+                System.out.println("Turn count = " + card);
                 System.out.println(playerOne + " has taken a turn.");
+                if (CardGame.playDeck.size() > 1 && CardGame.playDeck.get(CardGame.playDeck.size() - 1).getValue() == CardGame.playDeck.get(CardGame.playDeck.size() - 2).getValue()) {
+                    System.out.println("SNAP! " + playerOne + " wins the game!");
+                scanner.close();
+                }
             }
         }
     }
